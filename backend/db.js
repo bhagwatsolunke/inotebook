0a15d550162c0341595be8cd749bc2eb4657f3a0
bhagwat solunke <bhagwatsolunke007@gmail.com>
2023-02-28 01:43:46 +0530

    video 46 done

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose');
 
 
 
-const mongoURI = "mongodb://localhost:27017/?directConnection=true";
+const mongoURI = "mongodb://localhost:27017/inotebook?directConnection=true";
 
 const connectToMongo=()=>{
     mongoose.connect(mongoURI,()=>{
