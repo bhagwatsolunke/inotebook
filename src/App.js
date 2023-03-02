aaa571184bec17478a4b24af55691efb9df3bcbe
bhagwat solunke <bhagwatsolunke007@gmail.com>
2023-03-02 14:26:25 +0530

    Navbar and Router Applied v-56done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import { Home } from './Components/Home';
+import About from './Components/About';
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>This is iNotebook</h1>
+    <div>
+    <Router>
+      <Navbar/>
+     <Routes>
+     <Route exact path="/" element={<Home/>}/>
+     <Route exact path="/about" element={<About/>}/>
+     </Routes>
+     </Router>
     </div>
   );
 }
