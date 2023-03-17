728128d414067dc42dcf8b3298b8afa0f3f76163
bhagwat solunke <bhagwatsolunke007@gmail.com>
2023-03-17 17:49:48 +0530

    small changes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello Harry!')
+  res.send('Hello bhagwat!')
 })
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
