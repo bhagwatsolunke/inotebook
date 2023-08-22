56a57f5a75d4b67fe85447d6341ef1d8b4265946
bhagwatsolunke <bhagwatsolunke007@gmail.com>
2023-08-22 14:51:41 +0530

    Using mysql for CRUD operations

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,27 @@
-//from new branch
-const connectToMongo = require('./db');
-const express = require('express')
-var cors = require('cors')
-connectToMongo();
-const app = express()
-const port = 5000
-app.use(cors())
-app.use(express.json())
+const express = require('express');
+const sequelize = require('./db');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello bhagwat!')
-})
-// Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+  res.send('Hello bhagwat!');
+});
 
 
-app.listen(port, () => {
-  console.log(`iNotebook Backend listening at http://localhost:${port}`)
-})
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+
+// Sync Sequelize models with the database
+sequelize.sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`iNotebook Backend listening at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
+  });
