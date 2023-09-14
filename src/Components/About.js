284654c16d114989ff190433cc5c0c6f554b0806
bhagwatsolunke <bhagwatsolunke007@gmail.com>
2023-09-14 14:36:23 +0530

    About page added

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './About.css'; 
 
-
- const About = () => {
-   
-    
+const About = () => {
   return (
-    <div>
-        This is About page
+    <div className="about-container">
+      <h2 className="about-title">About Me</h2>
+      <p className="about-text">
+        My name is Bhagwat Madhukar Solunke, and I am a software engineering student.
+      </p>
+      <p className="about-text">
+        This is the About page of the iNotebook web application that I developed using the MERN stack.
+      </p>
+      <p className="about-text">
+        You can find the project's source code on my GitHub repository: 
+        <a className="github-link" href="https://github.com/bhagwatsolunke/inotebook" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+      </p>
     </div>
-  )
-}
-export default About
+  );
+};
+
+export default About;
